9e314dbd729e37e65fb4c66599a6a7847edc37b3
mikkoikoi <mikorapz12@gmail.com>
2022-10-11 10:41:10 +0700

    add some

---
+++ minggu2/arr(1).js
@@ -0,0 +1,9 @@
+let buah = ["semangka", "apel", "jambu"]
+buah[2] = "durian"
+console.log(buah)
+buah.push("rambutan", "apel")
+console.log(buah)
+buah.pop()
+console.log(buah)
+buah.splice(1, 2) 
+console.log(buah)
