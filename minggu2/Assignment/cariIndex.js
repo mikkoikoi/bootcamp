f37458e759a515ef3e671cb66029d7523197e77d
mikkoikoi <mikorapz12@gmail.com>
2022-10-13 11:30:41 +0700

    add

--- minggu2/Assignment/cariIndex.js
+++ minggu2/Assignment/cariIndex.js
@@ -1,14 +1,14 @@
 let merk = ["Ford", "Mercedes-Benz", "Nissan", "Volkswagen"]
 
 function cari_merk (cari_index, merk_dicari) {
- let ketemu 
+ let ketemu
  for(let i = 0; i <cari_index.length; i++) {
     if (cari_index[i] == merk_dicari) {
         ketemu = i
     }
  }
- return "ada " + merk_dicari + " di index ke - " + ketemu
+ return "ada " + merk_dicari + " di index ke " + ketemu
 }
 // let hasil
 // hasil = cari_merk(merk, "Nissan")
-console.log(cari_merk(merk, "Nissan"))
+console.log(cari_merk(merk, "Ford"))
--- minggu2/Assignment/multiple.js
+++ minggu2/Assignment/multiple.js
@@ -2,9 +2,9 @@ let arr = [4, 5, 7, 2, 3, 6, 13, 15, 2, 21]
 
 function arrBaru () {
     let arr2 = []
-    for (let element of arr) {
-        if(element % 3 === 0) {
-           arr2.push(element)
+    for (let angka of arr) {
+        if(angka % 3 === 0) {
+           arr2.push(angka)
         }
     }
     console.log(arr2)
