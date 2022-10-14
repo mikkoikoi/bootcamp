1ebb9bcaa8aadf81dc6e7f4e9e749e65926dae50
mikkoikoi <mikorapz12@gmail.com>
2022-10-14 13:40:06 +0700

    add

--- minggu2/factorial.js
+++ minggu2/factorial.js
@@ -1,9 +1,9 @@
-const factorial = function(start) {
-    if (start == 0) {
+const factorial = function(mulai) {
+    if (mulai == 0) {
         return 1
     }
     else {
-        return start * factorial(start - 1)
+        return mulai * factorial(mulai - 1)
     }
 }
 console.log(factorial(5))
