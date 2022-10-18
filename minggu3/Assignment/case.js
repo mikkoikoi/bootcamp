25dc76fcfa24325fb0b87d90cf8febec9c0e925e
mikkoikoi <mikorapz12@gmail.com>
2022-10-18 11:27:07 +0700

    add

--- minggu3/Assignment/case.js
+++ minggu3/Assignment/case.js
@@ -1,8 +1,21 @@
 // dengan map ada data angka pos output 
-
+const data = [1, 2, 3, 4, 5]
 // function (a,b) {
 //     let c = 2
 //     return a**b + c
 // }
-const c= 2
-const func =  (a,b) => a**b + c
+// const c= 2
+// const func =  (a,b) => a**b + c
+
+// const pangkat = data.map ((a) => {
+//     let c = 2
+//     return a**2 + c
+// })
+// console.log(pangkat)
+
+const pangkat = data.map  ((a) => {
+    let c = 5
+    return a**10 + c
+})
+
+console.log(pangkat)
