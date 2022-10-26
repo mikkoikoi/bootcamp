d0c6e32148f08a63557da655d3105a9ca9131fcd
mikkoikoi <mikorapz12@gmail.com>
2022-10-26 17:40:51 +0700

    add

--- minggu3/Assignment/dataMain/js/tampil.js
+++ minggu3/Assignment/dataMain/js/tampil.js
@@ -14,16 +14,16 @@
 function tampilData (data) {
     console.log(data)
     const tbodyElement = document.getElementById("databody")
-    const rows = window.userData.map((data, i)=>`<tr>
-    <td>${data.nik}</td>
-    <td>${data.nama}</td>
-    <td>${data.umur}</td>
-    <td>
-        <button class="button" data-edit="${data.nama}" id="btn1">Edit</button>
-        <button class="button button2" data-del="${data.nik}" id="btn2">Delete</button>
-    </td>
-        </tr>`).join("")
-        tbodyElement.innerHTML=rows
+    // const rows = window.userData.map((data, i)=>`<tr>
+    // <td>${data.nik}</td>
+    // <td>${data.nama}</td>
+    // <td>${data.umur}</td>
+    // <td>
+    //     <button class="button" data-edit="${data.nama}" id="btn1">Edit</button>
+    //     <button class="button button2" data-del="${data.nik}" id="btn2">Delete</button>
+    // </td>
+    //     </tr>`).join("")
+    //     tbodyElement.innerHTML=rows
 }
 
 export{tampilData}
