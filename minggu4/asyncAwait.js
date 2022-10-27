f7152955879d6b3a1ed881302b2f6202826dda64
mikkoikoi <mikorapz12@gmail.com>
2022-10-27 11:27:59 +0700

    add

--- minggu4/asyncAwait.js
+++ minggu4/asyncAwait.js
@@ -1,17 +1,22 @@
 function cobaPromise () {
-    return new Promise (resolve => {
+    return new Promise ((resolve, reject) => {
         const waktu = 5500
         if(waktu < 5000) {
         setTimeout(() => {
             resolve('selesai')
         }, 2000)
         } else {
-            PromiseRejectionEvent('lama')
-            console.log(PromiseRejectionEvent.promise)
-            console.log(PromiseRejectionEvent.reason)
+            // PromiseRejectionEvent('lama')
+            // console.log(PromiseRejectionEvent.promise)
+            // console.log(PromiseRejectionEvent.reason)
+            // reject ('lama')
+            let i = document.getElementById('btn')
+            i.addEventListener('unhandledrejection', alert('halo'))
         }
     })
 }
+
+// const coba = cobaPromise
 
 async function cobaAsync () {
     const coba = await cobaPromise()
