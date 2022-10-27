8d096e2c8c87ad5fe7c3021c7bdbb2828982806b
mikkoikoi <mikorapz12@gmail.com>
2022-10-27 11:37:04 +0700

    add

--- minggu4/asyncAwait.js
+++ minggu4/asyncAwait.js
@@ -1,30 +1,37 @@
-function cobaPromise () {
-    return new Promise ((resolve, reject) => {
-        const waktu = 5500
-        if(waktu < 5000) {
-        setTimeout(() => {
-            resolve('selesai')
-        }, 2000)
-        } else {
-            // PromiseRejectionEvent('lama')
-            // console.log(PromiseRejectionEvent.promise)
-            // console.log(PromiseRejectionEvent.reason)
-            // reject ('lama')
-            let i = document.getElementById('btn')
-            i.addEventListener('unhandledrejection', alert('halo'))
-        }
-    })
+function cobaPromise() {
+  return new Promise((resolve, reject) => {
+    const waktu = 5500;
+    if (waktu < 5000) {
+      setTimeout(() => {
+        resolve("selesai");
+      }, 2000);
+    } else {
+      // PromiseRejectionEvent('lama')
+      // console.log(PromiseRejectionEvent.promise)
+      // console.log(PromiseRejectionEvent.reason)
+      // reject ('lama')
+      reject();
+    }
+  });
 }
 
 // const coba = cobaPromise
 
-async function cobaAsync () {
-    const coba = await cobaPromise()
-    console.log(coba)
+async function cobaAsync() {
+  const coba = await cobaPromise()
+    .then((resolve) => {
+      resolve();
+    })
+    .catch((reject) => {
+      console.log(reject)
+    });
 }
 
+let i = document.getElementById("btn");
+i.addEventListener("unhandledrejection", alert("halo"));
+
 try {
-    cobaAsync()
-    } catch (e) {
-        alert(e)
-    }
+  cobaAsync();
+} catch (e) {
+  alert(e);
+}
