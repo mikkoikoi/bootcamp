d3556712a3ae544cce4a389dee88f119787db292
mikkoikoi <mikorapz12@gmail.com>
2022-10-28 14:14:48 +0700

    add

---
+++ minggu4/halo.js
@@ -0,0 +1,17 @@
+let arr = [2, 1, 3, 5, 6, 9]; 
+let n = arr.length;
+
+function insertionSort(arr, n) {
+let i, key, j
+for (i = 1; i <  n; i++) {
+    key=arr[i]
+    j = i-1
+    while (j >= 0 && arr[j] > key) {
+        arr[j+1] = arr[j]
+        j = j-1
+    }
+    arr[j+1] = key
+   console.log(arr)
+    }
+}
+console.log(insertionSort(arr, n));
